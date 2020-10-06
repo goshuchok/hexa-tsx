f1dd0e8f7a693d3b95ed88d27d55bf4a306c9a87
Igor Povar <goshuchok@gmail.com>
2020-10-06 23:58:45 +0300

    added sass

---
+++ src/components/header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <span>Hexalogo</span>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Work</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
